24af32f7220c60dc60d49a6d6e6e73c6d7454622
Daniel Tabin <dieltabin@gmail.com>
2017-07-19 14:11:05 -0600

    Continued to add css.  Starting a navbar

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -4,11 +4,15 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace MachineUpdateFrontEnd
 {
     public partial class Default : System.Web.UI.Page
     {
+        SqlConnection con = new SqlConnection("Data Source = 151728A\\SQLEXPRESS; Initial Catalog = MachineUpdateDataBase; Integrated Security = True; Connect Timeout = 60; Encrypt=False;TrustServerCertificate=True");
+        
         protected void Page_Load(object sender, EventArgs e)
         {
 
