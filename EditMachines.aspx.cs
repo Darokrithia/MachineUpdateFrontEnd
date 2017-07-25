ce61c3676a1e7557a658d625494436c955928ed6
Daniel Tabin <dieltabin@gmail.com>
2017-07-25 16:19:23 -0600

    Adding data now works

--- EditMachines.aspx.cs
+++ EditMachines.aspx.cs
@@ -31,7 +31,19 @@ namespace MachineUpdateFrontEnd
 
         protected void SubmitButtonClick(object sender, EventArgs e)
         {
-
+            SqlDataSource1.InsertParameters["MachineName"].DefaultValue =
+                ((TextBox)MachineGrid.FooterRow.FindControl("MachineNameEntryTextBox")).Text;
+            SqlDataSource1.InsertParameters["MachineIP"].DefaultValue =
+                ((TextBox)MachineGrid.FooterRow.FindControl("MachineIPEntryTextBox")).Text;
+            SqlDataSource1.InsertParameters["MachineOSID"].DefaultValue =
+                ((DropDownList)MachineGrid.FooterRow.FindControl("OSEntryDropDownList")).SelectedValue;
+            SqlDataSource1.InsertParameters["MachineTypeID"].DefaultValue =
+                ((DropDownList)MachineGrid.FooterRow.FindControl("MachineTypeEntryDropDown")).SelectedValue;
+            SqlDataSource1.InsertParameters["Virtual"].DefaultValue =
+                ((CheckBox)MachineGrid.FooterRow.FindControl("VirtualEntryCheckBox")).Checked.ToString();
+            SqlDataSource1.InsertParameters["MachineHostID"].DefaultValue =
+                ((DropDownList)MachineGrid.FooterRow.FindControl("HostEntryDropDown")).SelectedValue;
+            SqlDataSource1.Insert();
         }
     }
 }
